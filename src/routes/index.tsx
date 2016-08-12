7fbcec4f9e90946be8081cb3a2d173fdcc519cc7
Hicham El Hammouchi <hicham.elhammouchi@gmail.com>
2016-08-12 01:10:51 -0400

    Updated entry point to use views, components and routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,22 @@
 import * as React from 'react';
 import { IndexRoute, Route } from 'react-router';
-import Layout from '../layout';
-import About from '../views/about';
-import Home from '../views';
-import NotFound from '../views/404';
+
+import {Hello} from '../components/Hello';
+import {Layout} from '../layout';
+import {About} from '../views/about';
+import {Home} from '../views';
+import {NotFound} from '../views/404';
+
+const Component = (compiler: string, framework: string) => {
+  return <Hello compiler={compiler} framework={framework} />;
+};
 
 export default (
   <Route path="/" component={Layout}>
     <IndexRoute component={Home} />
 
     <Route path="/about" component={About} />
+    <Route path="/hello" component={Component.bind(this, 'TypeScript', 'React')} />
 
     <Route path="*" component={NotFound} />
   </Route>
