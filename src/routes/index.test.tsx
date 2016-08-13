151b504f449bbc380519ed69eba39371875a8eb5
Hicham El Hammouchi <hicham.elhammouchi@gmail.com>
2016-08-12 23:15:39 -0400

    Added route test

--- src/routes/index.test.tsx
+++ src/routes/index.test.tsx
@@ -1,9 +1,23 @@
 import * as React from 'react';
 import * as test from 'tape';
-import { createRenderer } from 'react-addons-test-utils';
 
+import { render } from 'react-dom';
+import { Router, createMemoryHistory } from 'react-router';
+import Routes from '../routes';
 
-test('Test Routes', (t: test.Test) : void => {
+require('jsdom-global')();
+
+test.only('Test Routes', (t: test.Test) : void => {
+  const node = document.createElement('div');
+  const Component = (
+    <Router history={createMemoryHistory('/hello')}>
+      {Routes}
+    </Router>
+  );
+
+  render((Component), node, () => {
+    t.notEqual(node.textContent.indexOf('This page uses TypeScript and React!'), -1, '404 reached');
+  });
 
   t.end();
 });
