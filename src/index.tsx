fa55a13cbef4319e8ab69264c2b295677ad895d5
Hicham El Hammouchi <hicham.elhammouchi@gmail.com>
2016-08-12 23:14:45 -0400

    minor update

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,10 @@ import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import Routes from './routes';
 
-const component = (
+const Component = (
   <Router history={browserHistory}>
     {Routes}
   </Router>
 );
 
-render(component, document.getElementById('example'));
+render(Component, document.getElementById('example'));
