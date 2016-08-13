bfa4c68b7861a506aae1c2e2935bc92bb81a4125
Hicham El Hammouchi <hicham.elhammouchi@gmail.com>
2016-08-13 04:38:32 -0400

    updated for 100% coverage

--- src/routes/index.test.tsx
+++ src/routes/index.test.tsx
@@ -5,18 +5,34 @@ import { render } from 'react-dom';
 import { Router, createMemoryHistory } from 'react-router';
 import Routes from '../routes';
 
-require('jsdom-global')();
+require('jsdom-global')(); // Used to make available the DOM element document
 
-test.only('Test Routes', (t: test.Test) : void => {
-  const node = document.createElement('div');
-  const Component = (
-    <Router history={createMemoryHistory('/hello')}>
-      {Routes}
-    </Router>
-  );
+const Routing = (
+  <Router history={createMemoryHistory('/')}>
+    {Routes}
+  </Router>
+);
 
-  render((Component), node, () => {
-    t.notEqual(node.textContent.indexOf('This page uses TypeScript and React!'), -1, '404 reached');
+test('Test Routes', (t: test.Test) : void => {
+  let node = document.createElement('div');
+
+  render((Routing), node, () => {
+    t.notEqual(node.textContent.indexOf('Index'), -1, 'Index page found');
+  });
+
+  Routing.props.history.push('/hello');
+  render((Routing), node, () => {
+    t.notEqual(node.textContent.indexOf('This page uses TypeScript and React!'), -1, 'Hello page found');
+  });
+
+  Routing.props.history.push('/about');
+  render((Routing), node, () => {
+    t.notEqual(node.textContent.indexOf('About'), -1, 'About page found');
+  });
+
+  Routing.props.history.push('/doesnotexist');
+  render((Routing), node, () => {
+    t.notEqual(node.textContent.indexOf('404'), -1, '404 page found');
   });
 
   t.end();
