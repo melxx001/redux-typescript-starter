e59ad431717d133e777caeebdc9b67a3a4a0c3eb
Hicham El Hammouchi <hicham.elhammouchi@gmail.com>
2016-08-18 04:26:37 -0400

    Updated entry point

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,28 @@
 import * as React from 'react';
-import { render } from 'react-dom';
-import { Router, browserHistory } from 'react-router';
+import {Provider} from 'react-redux';
+import {render} from 'react-dom';
+import {Router, useRouterHistory} from 'react-router';
+import {createHashHistory} from 'history';
+import {syncHistoryWithStore} from 'react-router-redux';
+
+
+import configureStore from './store';
+
 import Routes from './routes';
 
+const store = configureStore();
+
+// Needed to add the "any" type to prevent compilation errors
+const createHistory: any = createHashHistory;
+const appHistory: any = useRouterHistory(createHistory)({ queryKey: false });
+const history: any = syncHistoryWithStore(appHistory, store);
+
 const Component = (
-  <Router history={browserHistory}>
-    {Routes}
-  </Router>
+  <Provider store={store} key="provider">
+    <Router history={history}>
+      {Routes}
+    </Router>
+  </Provider>
 );
 
 render(Component, document.getElementById('example'));
