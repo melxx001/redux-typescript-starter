bf3ecae8a35c2a9d98de7dd185e9108bdd51d8ce
Hicham El Hammouchi <hicham.elhammouchi@gmail.com>
2016-09-21 07:12:59 -0400

    updates to use browserHistory and have better urls

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,16 @@
 import * as React from 'react';
 import {Provider} from 'react-redux';
 import {render} from 'react-dom';
-import {Router, useRouterHistory} from 'react-router';
-import {createHashHistory} from 'history';
-import {syncHistoryWithStore} from 'react-router-redux';
+import {Router, browserHistory} from 'react-router';
 
 import configureStore from './store';
 import Routes from './routes';
 
 const store = configureStore();
 
-// Needed to add the "any" type to prevent compilation errors
-const createHistory: any = createHashHistory;
-const appHistory: any = useRouterHistory(createHistory)({ queryKey: false });
-const history: any = syncHistoryWithStore(appHistory, store);
-
 const Component = (
   <Provider store={store} key="provider">
-    <Router history={history}>
-      {Routes}
-    </Router>
+    <Router history={browserHistory} routes={Routes} />
   </Provider>
 );
 
