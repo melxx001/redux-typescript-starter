6e4d786334d0bcf6057e074f4ba33798142cb8e8
Hicham El Hammouchi <hicham.elhammouchi@gmail.com>
2016-09-22 06:45:55 -0400

    Added ToDo to root reducer

--- src/rootReducer.ts
+++ src/rootReducer.ts
@@ -1,8 +1,18 @@
-import {combineReducers} from 'redux';
+import {combineReducers, Reducer} from 'redux';
 import {routerReducer} from 'react-router-redux';
 import {counterReducer} from './components/reducers/counter';
+import {todoReducer, visibilityFilterReducer} from './components/reducers/todo';
+
+export interface ReducerState {
+  todoReducer: Array<any>;
+  visibilityFilterReducer: string;
+  counterReducer: Number;
+  routing: Reducer<any>;
+}
 
 export default combineReducers({
+  todoReducer,
+  visibilityFilterReducer,
   counterReducer,
   routing: routerReducer
 });
